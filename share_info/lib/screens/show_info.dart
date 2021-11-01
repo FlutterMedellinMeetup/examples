a90b83c47c922507ca72543101c3a9f5265fd6c5
daniherr <daniherr@bancolombia.com.co>
2021-11-01 18:25:06 -0500

    sd

--- share_info/lib/screens/show_info.dart
+++ share_info/lib/screens/show_info.dart
@@ -2,14 +2,22 @@ import 'package:flutter/material.dart';
 
 class ShowInfo extends StatelessWidget {
   late final String title;
+  late final String subTitle;
   late final String description;
   ShowInfo({required this.title, required this.description});
   @override
   Widget build(BuildContext context) {
     Widget _helloPage = Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(
+          title: Text(
+        title,
+        textScaleFactor: 2,
+      )),
       body: Center(
-        child: Text(description),
+        child: Column(children: [
+          Text(subTitle),
+          Text(description),
+        ]),
       ),
     );
     return _helloPage;
